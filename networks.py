3b8ddf526554557eb9365b4980b184dea0ffdfd7
Raul Steinmetz <rsteinmetz@inf.ufsm.br>
2022-09-19 21:06:25 -0300

    value network and critic network squeleton

--- networks.py
+++ networks.py
@@ -1,18 +1,87 @@
-# What is a Soft Actor Critic? Is a Deep Reinforcement Learning composed by two main neural networks. One of those is
-# the agent, which will learn to chose on actions given a certain state, considering the probability of the state
-# that will result from that action, and the possible states that can occur giving this new state. The other DNN
-# is the critic, which will specialize in judging the agent on its actions (calculating the reward function), given
-# the probability of the state that will be caused by tha t action, and the possible states that will come after.
-# There will also be a third network, used for controlling given a state, which actions are possible (valuable).
+import os  # handle model checkpointing (saving)
+import torch as T
+import torch.nn.functional as F
+import torch.nn as nn
+import torch.optim as optim
+from torch.distributions.normal import Normal
+import numpy as np
 
-# What do we want? Get robots to learn with stability in a continuous space environment
 
-# Why is SAC a good option for our V3S robots? It can learn way faster than the other conventional
-# reinforcement learning deep neural networks, and since we could not find a way to accelerate the
-# simulator, this fits perfectly. Other than that, actor critic networks seem to achieve very good
-# results, since the cost function used is complemented with a high entropy (stochasticity), which gives us a better
-# chance to reach the global optima (encourages exploration), for example. It maximizes the reward over time
+# Critic
+class CriticNetwork(nn.Module):
+    # builder
+    def __init__(self, beta, input_dims, n_actions,fc1_dims=256, fc2_dims=256, name='critic',chkpt_dir='tmp/sac'):
+        super(CriticNetwork, self).__init__()
+        self.input_dims = input_dims
+        self.fc1_dims = fc1_dims
+        self.fc2_dims = fc2_dims
+        self.n_actions = n_actions
+        self.name = name
+        self.checkpoint_dir = chkpt_dir
+        self.checkpoint_file = os.path.join(self.checkpoint_dir, name+'_sac')
 
-# How does SAC differ from its pairs? Other than its max-entropy model, there is a very important difference in the
-# SAC network. The output from the other models will be direct, while SAC outputs an average and a standard deviation,
-# which will form a normal distribution, which will be sampled to choose the action.
+        # network
+        self.fc1 = nn.Linear(self.input_dims[0]+n_actions, self.fc1_dims)
+        self.fc2 = nn.Linear(self.fc1_dims, self.fc2_dims)
+        self.q = nn.Linear(self.fc2_dims, 1)  # output
+
+        self.optimizer = optim.Adam(self.parameters(), lr=beta)
+        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')  # will train in gpu if possible
+
+        self.to(self.device)
+
+    def forward(self, state, action):
+        action_value = self.fc1(T.cat([state, action], dim=1))
+        action_value = F.relu(action_value)  # activating action value
+        action_value = self.fc2(action_value)
+        action_value = F.relu(action_value)
+
+        q = self.q(action_value)
+
+        return q
+
+    def save_checkpoint(self):
+        T.save(self.state_dict(), self.checkpoint_file)
+
+    def load_checkpoint(self):
+        self.load_state_dict(T.load(self.checkpoint_file))
+
+
+# value network
+class ValueNetwork(nn.Module):
+    def __init__(self, beta, input_dims, fc1_dims=256, fc2_dims=256, name='value', chkpt_dir='tmp/sac'):
+        super(ValueNetwork, self).__init__()
+        self.input_dims = input_dims
+        self.fc1_dims = fc1_dims
+        self.fc2_dims = fc2_dims
+        self.name = name
+        self.checkpoint_dir = chkpt_dir
+        self.checkpoint_file = os.path.join(self.checkpoint_dir, name+'_sac')
+
+        self.fc1 = nn.Linear(*self.input_dims, self.fc1_dims)
+        self.fc2 = nn.Linear(self.fc1_dims, fc2_dims)
+        self.v = nn.Linear(self.fc2_dims, 1)
+
+        self.optimizer = optim.Adam(self.parameters(), lr=beta)
+        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
+
+        self.to(self.device)
+
+    def forward(self, state):
+        state_value = self.fc1(state)
+        state_value = F.relu(state_value)
+        state_value = self.fc2(state_value)
+        state_value = F.relu(state_value)
+
+        v = self.v(state_value)
+
+        return v
+
+    def save_checkpoint(self):
+        T.save(self.state_dict(), self.checkpoint_file)
+
+    def load_checkpoint(self):
+        self.load_state_dict(T.load(self.checkpoint_file))
+
+
+# Actor network
